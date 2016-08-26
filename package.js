383d57f2f38d61389edb611145a311a44d5c46e0
vishal sharma <v.sharma@indy-cloud.com>
2016-08-26 16:21:31 +0700

    resolve error

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'indycloud:indycsv',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Collection to csv Export',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/INDYvishal/indycsv.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
